e73422cd8dfd562179c4d137607d2844e0af72a2
SharonMaiyo44 <sharonmaiyo44@gmail.com>
2025-04-06 23:11:35 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,14 @@
+print("hello there!")
+
+
+
+("greetings.py")
+site_name = "Power Learn Project"
+print(site_name)
+site_name = "I love coding 😊"
+print(site_name)
+a,b,c = 5, 7, "Hello world"
+print(a)
+print (b)
+print (c)
+type (2)
